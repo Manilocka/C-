a5b8809c7002f31f3e0ec347fc93f472c9470a9d
Manilocka <ragozina2003@gmail.com>
2024-10-12 18:54:13 +0300

    upgrade_1.3

--- 1.3_task.cs
+++ 1.3_task.cs
@@ -4,7 +4,7 @@ class SecondTask
 {
     static void Main(string[] args)
     {
-        
+
         Console.WriteLine("Коэффициент a:");
         double a = Convert.ToDouble(Console.ReadLine());
 
@@ -13,48 +13,46 @@ class SecondTask
 
         Console.WriteLine("Коэффициент c:");
         double c = Convert.ToDouble(Console.ReadLine());
+        
 
         SolveEquation(a, b, c);
     }
 
     static void SolveEquation(double a, double b, double c)
     {
-        if (a != 0) 
+        if (a == 0)
         {
-            double discriminant = b * b - 4 * a * c;
-
-            if (discriminant > 0)
+            if (b == 0)
             {
-                
+                if (c == 0)
+                {
+                    Console.WriteLine("Уравнение имеет бесконеееееееечно много решений (0 = 0).");
+                }
+                else
+                {
+                    Console.WriteLine("Нет действительных корней(((");
+                }
+            }
+            else
+            {
+                double x = -c / b;
+                Console.WriteLine($"Уравнение имеет лишь один корень: x = {x}");
+            }
+        }
+        else
+        {
+            if ((Math.Pow(b, 2) - 4 * c * a) < 0)
+            {
+                Console.WriteLine("Нет решений((((");
+            }
+            else
+            {
+                double discriminant = b * b - 4 * a * c;
                 double x1 = (-b + Math.Sqrt(discriminant)) / (2 * a);
                 double x2 = (-b - Math.Sqrt(discriminant)) / (2 * a);
                 Console.WriteLine($"Уравнение имеет два!!! различных корня: x1 = {x1}, x2 = {x2}");
             }
-            else if (discriminant == 0) 
-            {
-                double x = -b / (2 * a);               
-                Console.WriteLine($"Уравнение имеет лишь один корень: x = {x}");
-            }
-            else
-            {
-                Console.WriteLine("Нет действительных корней(((");
-            }
         }
-        else if (b != 0) 
-        {
-            double x = -c / b;
-            Console.WriteLine($"Уравнение имеет лишь один корень: x = {x}");
-        }
-        else 
-        {
-            if (c == 0)
-            {
-                Console.WriteLine("Уравнение имеет бесконеееееееечно много решений (0 = 0).");
-            }
-            else
-            {
-                Console.WriteLine("Уравнение не имеет решений(((");
-            }
-        }
+
     }
 }
